17b970c87a600954f52bd8b74c6dacdd683b73a7
jimmytu <jimmytutron@gmail.com>
2018-04-26 18:59:21 -0700

    Create event for when keys are pressed, start logging guesses left, wins, losses.

--- assets/game.js
+++ assets/game.js
@@ -7,7 +7,7 @@ var wins = 0 ;
 var losses = 0;
 var guesses = 10;
 var guessesLeft = 10;
-var guessAttempts = [];
+var guessesMade = [];
 var playerGuess = null;
 
 //generate the computer's guess using a random letter from the letters array
@@ -20,16 +20,37 @@ var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 var winText = $("#wins");
 var lossText = $("#losses");
 var guessesLeftText = $("#Guesses-Left");
-var guessesMade = $("#Guesses-Made");
+var guessesMadeText = $("#Guesses-Made");
 
+//Fill the empty divs with content, in this case the number of wins,loses, etc;
+winText.html(wins);
+lossText.html(losses);
+guessesLeftText.html(guessesLeft);
+guessesMadeText.html(guessesMade);
 
 //when the user presses a key, log the key pressed in a string
 document.onkeyup = function(keyPress) {
 
+	guessesMadeText.html(guessesMade);
+	guessesLeft--;
+	guessesLeftText.html(guessesLeft);
+
 	var userGuess = (keyPress.key).toLowerCase();
 	console.log(userGuess);
+	console.log(guessesLeft);
 
-
+	if (guessesLeft > 0){
+		if (userGuess === computerGuess){
+			wins++;
+			winText.html(wins);
+		$("#winner").html("<h2>wow. you won. *claps* </h2>")
+		}
+	}
+	else if (guessesLeft === 0){
+		losses++;
+		lossText.html(losses);
+		$("#loser").html("<h2>loooooser!!! ya lost boiiii</h2>")
+	}
 }
 
 //Keep track of user's guess attempts and makes sure the guesses are actually letters
