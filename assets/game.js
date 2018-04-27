2a171fb5e16ed279c933800f78676d26623c6fa1
jimmytu <jimmytutron@gmail.com>
2018-04-26 20:13:58 -0700

    IT WORKSgit add .git add .git add . #BLESS

--- assets/game.js
+++ assets/game.js
@@ -5,54 +5,64 @@ var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','
 
 var wins = 0 ;
 var losses = 0;
-var guesses = 10;
 var guessesLeft = 10;
 var guessesMade = [];
-var playerGuess = null;
 
 //generate the computer's guess using a random letter from the letters array
 
 var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-
-// make sure it works hehe console.log(computerGuess);
+//cheat code hehe open console log for guaranteed win!
+console.log(computerGuess);
 
 // call the ID of the divs within the HTML
 var winText = $("#wins");
 var lossText = $("#losses");
 var guessesLeftText = $("#Guesses-Left");
 var guessesMadeText = $("#Guesses-Made");
+var result = $("#result");
 
-//Fill the empty divs with content, in this case the number of wins,loses, etc;
+//Fill the empty divs with content, in this case the number of wins,loses, etc; at default
 winText.html(wins);
 lossText.html(losses);
 guessesLeftText.html(guessesLeft);
 guessesMadeText.html(guessesMade);
 
 
+//A function to reset the values once the user wins or loses
 var reset = function() {
- guesses = 10;
  guessesLeft = 10;
  guessesMade = [];
  guessesLeftText.html(guessesLeft);
- guessesMadeText.html(guessesMade);
+ computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
 }
 
 //when the user presses a key, log the key pressed in a string
 document.onkeyup = function(keyPress) {
 
-	guessesMadeText.html(guessesMade);
+	//subtract the amount of guesses left after every key press
 	guessesLeft--;
+	//update that value in the div
 	guessesLeftText.html(guessesLeft);
 
+
+	//set the key presses to lowercase in case user has caplocks on or something
 	var userGuess = (keyPress.key).toLowerCase();
-	console.log(userGuess);
-	console.log(guessesLeft);
+
+	//TEST TEST
+	// console.log(userGuess);
+	// console.log(guessesLeft);
+
+	guessesMade.push(userGuess);
+	guessesMadeText.html(guessesMade.join(", "));
+
+
 
 	if (guessesLeft > 0){
 		if (userGuess === computerGuess){
 			wins++;
 			winText.html(wins);
-		$("#winner").html("<h2>wow. you won. *claps* </h2>");
+		result.html("<h3>wow. you won. *claps* </h3> the letter was... " + computerGuess);
 		reset();
 		}
 
@@ -60,7 +70,7 @@ document.onkeyup = function(keyPress) {
 	else if (guessesLeft === 0){
 		losses++;
 		lossText.html(losses);
-		$("#loser").html("<h2>loooooser!!! ya lost boiiii</h2>")
+		result.html("<h3>loooooser!!! ya lost boiiii</h3> the letter was... " + computerGuess);
 		reset();
 	}
 }
